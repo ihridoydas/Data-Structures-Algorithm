150c418644ecf39183740f90c4b93c57b79110fe
Hridoy Chandra Das <32935576+ihridoydas@users.noreply.github.com>
2021-08-09 17:41:22 +0900

    Update quickSort_dec.java

--- quickSort_dec.java
+++ quickSort_dec.java
@@ -1,46 +1,60 @@
-public class quickSort_dec {
-
-    private static void QuickSort(int[] arr, int left, int right) {     
-        int index = partition(arr, left, right);
-
-        if (left < index - 1)
-            QuickSort(arr, left, index - 1);
-        if (index + 1 < right)
-            QuickSort(arr, index + 1, right);
-
-    }
-
-    private static int partition(int[] arr, int left, int right) {
-        int pivot = arr[left];
-
-        while (left <= right) {
-            while (arr[left] < pivot)
-                left++;
-            while (arr[right] > pivot)
-                right--;
-
-            if (left <= right) {
-                int temp = arr[left];
-                arr[left] = arr[right];
-                arr[right] = temp;
-
-                left++;
-                right--;
-            }
-
+import java.util.Arrays;
+public class quickSort_dec
+{
+   public static void quickSort(Integer[] array, int lowest, int highest) 
+       {
+         if (array == null || array.length == 0){
+             return;
+         }
+         if (lowest >= highest){
+             return;
+         }
+  
+         int middle = lowest + (highest - lowest) / 2;
+         int pivot = array[middle];
+ 
+         int i = lowest, j = highest;
+         while (i <= j)
+         {
+             while (array[i] < pivot) 
+             {
+                 i++;
+             }
+             while (array[j] > pivot) 
+             {
+                 j--;
+             }
+             if (i <= j) 
+             {
+                 swap (array, i, j);
+                 i++;
+                 j--;
+             }
+         }
+    
+         if (lowest < j)
+         {
+             quickSort(array, lowest, j);
+         }
+         if (highest > i)
+         {
+             quickSort(array, i, highest);
+         }
+     }
+      
+     public static void swap (Integer array[], int x, int y)
+     {
+         int swapArray = array[x];
+         array[x] = array[y];
+         array[y] = swapArray;
+     }
+     
+     public static void main(String[] args) 
+     {
+         Integer[] testArray = new Integer[] { 5,24,2,45,5,20,99,56,5,101,2,53,12,20};
+         quickSort( testArray, 0, testArray.length - 1 );
+        for(int i=0;i<testArray.length;i++){
+            System.out.print(testArray[i] + "  ");
         }
-
-        return left;
-    }
-
-    public static void main(String[] args) {
-
-        int[] array = new int[] { 24,2,45,20,56,99,2,56,101,53,12};
-
-        QuickSort(array, 0, array.length - 1);
-        for (int i = 0; i < array.length - 1;i++) {
-            System.out.print(array[i] + "  ");
-        }
-
-    }
-}
+     }
+ }
