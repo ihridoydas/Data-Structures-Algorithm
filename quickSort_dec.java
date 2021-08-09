7678d0d24bbe6bef48090b0032617cde08eb72d2
Hridoy Chandra Das <32935576+ihridoydas@users.noreply.github.com>
2021-08-09 18:19:59 +0900

    Update quickSort_dec.java

--- quickSort_dec.java
+++ quickSort_dec.java
@@ -1,60 +1,61 @@
 
-public class quickSort_dec
-{
-   public static void quickSort(Integer[] array, int lowest, int highest) 
-       {
-         if (array == null || array.length == 0){
-             return;
-         }
-         if (lowest >= highest){
-             return;
-         }
-  
-         int middle = lowest + (highest - lowest) / 2;
-         int pivot = array[middle];
- 
-         int i = lowest, j = highest;
-         while (i <= j)
-         {
-             while (array[i] > pivot) 
-             {
-                 i++;
-             }
-             while (array[j] < pivot) 
-             {
-                 j--;
-             }
-             if (i <= j) 
-             {
-                 swap (array, i, j);
-                 i++;
-                 j--;
-             }
-         }
-    
-         if (lowest < j)
-         {
-             quickSort(array, lowest, j);
-         }
-         if (highest > i)
-         {
-             quickSort(array, i, highest);
-         }
-     }
-      
-     public static void swap (Integer array[], int x, int y)
-     {
-         int swapArray = array[x];
-         array[x] = array[y];
-         array[y] = swapArray;
-     }
-     
-     public static void main(String[] args) 
-     {
-         Integer[] testArray = new Integer[] { 5,24,2,45,5,20,99,56,5,101,2,53,12,20};
-         quickSort( testArray, 0, testArray.length - 1 );
-        for(int i=0;i<testArray.length;i++){
-            System.out.print(testArray[i] + "  ");
+public class quickSort_dec {
+    public static void quickSort(int array[], int left, int right) {
+
+        if (array == null || array.length == 0) {
+            return;
         }
-     }
- }
+        if (left >= right) {
+            return;
+        }
+
+        // start partitiion
+
+        // if are want ascending order
+
+        // while (array[i] < pivot)
+        //     i++;
+        // while (array[j] > pivot)
+        //     j--;
+
+        int pivot = array[left];
+
+        int i = left, j = right;
+        while (i <= j) {
+            while (array[i] > pivot)
+                i++;
+            while (array[j] < pivot)
+                j--;
+
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        // end of partition
+
+        // start of quick sort
+
+        if (left < j) {
+            quickSort(array, left, j);
+        }
+        if (right > i) {
+            quickSort(array, i, right);
+        }
+        // end of quick sort
+    }
+
+    // Main Mothod
+
+    public static void main(String[] args) {
+        int[] array = { 5, 24, 2, 45, 5, 20, 99, 56, 5, 101, 2, 53, 12, 20 };
+        quickSort(array, 0, array.length - 1);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "  ");
+        }
+    }
+}
